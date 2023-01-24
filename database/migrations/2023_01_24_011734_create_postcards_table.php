b8b024a49b60218dc332a7b64ed66238659be77a
zbowl <zbowl1@gmail.com>
2023-01-23 18:59:00 -0800

    postcards migration for future feature

---
+++ database/migrations/2023_01_24_011734_create_postcards_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('postcards', function (Blueprint $table) {
+            $table->id();
+            $table->string('recipient_name');
+            $table->string('street_1');
+            $table->string('street_2');
+            $table->string('city');
+            $table->string('state');
+            $table->string('zip');
+            $table->text('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('postcards');
+    }
+};

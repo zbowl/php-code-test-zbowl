5991c7065440eb08f147bc82b6d9336ec828d306
zbowl <zbowl1@gmail.com>
2023-01-23 19:00:05 -0800

    vue layout component

---
+++ resources/js/Layouts/AppLayout.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <div class="min-h-screen flex flex-col bg-gray-100">
+        <!-- Navigation -->
+        <div class="p-10 bg-gray-400">
+            <Link :href="route('postcards.create')">
+                Create Postcard
+            </Link>
+        </div>
+
+        <div
+            class="w-full mt-6 px-6 py-4 bg-gray-200 shadow-md sm:rounded-lg"
+        >
+            <slot />
+        </div>
+    </div>
+</template>
